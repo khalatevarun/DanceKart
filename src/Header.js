2da4ade11b6ecea4140a73957164776e30f9201f
Varun Khalate <khalatevarun@gmail.com>
2021-08-21 20:46:04 +0530

    add: styling to header layout and cart icon

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './Header.css';
 import amazon_logo from '../src/assets/amazon_logo.png';
+import SearchIcon from '@material-ui/icons/Search';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 
 const Header = () => {
   return (
@@ -8,6 +10,7 @@ const Header = () => {
       <img src={amazon_logo} className="header__logo" alt="" />
       <div className="header__search">
         <input className="header__searchInput" type="text" />
+        <SearchIcon className="header__searchIcon" />
       </div>
 
       <div className="header__nav">
@@ -22,6 +25,11 @@ const Header = () => {
         <div className="header__option">
           <span className="header__optionLineOne">Your</span>
           <span className="header__optionLineTwo">Prime</span>
+        </div>
+
+        <div className="header__optionBasket">
+          <ShoppingCartIcon />
+          <span className="header__optionLineTwo header__basketCount">0</span>
         </div>
       </div>
     </div>
