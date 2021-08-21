73729e8c3f54a1dd3f519757e8e7b12904ede734
Varun Khalate <khalatevarun@gmail.com>
2021-08-21 12:07:01 +0530

    add: firebase inital config file and remove unnecessary files

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyBd0pzaau_CpPaQgRdoP761E0xZabPJKgY',
+  authDomain: 'clone-f8a32.firebaseapp.com',
+  projectId: 'clone-f8a32',
+  storageBucket: 'clone-f8a32.appspot.com',
+  messagingSenderId: '933602471905',
+  appId: '1:933602471905:web:d66b06478bc42a25e92ca0',
+};
