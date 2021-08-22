6495e546793dbe1430d7595659c77cac2688d33d
Varun Khalate <khalatevarun@gmail.com>
2021-08-22 12:50:20 +0530

    add: dummy data and make rows for each category

--- src/Home.js
+++ src/Home.js
@@ -8,13 +8,105 @@ function Home() {
     <div className="home">
       <div className="home__container">
         <img className="home__image" src={banner} alt="" />
+        <div className="home__category">Jackets & Hoodies</div>
         <div className="home__row">
-          <Product />
-          <Product />
-          <Product />
+          <Product
+            image="//lp2.hm.com/hmgoepprod?set=source[/c3/08/c308e8eae26682c548bf8be6608bf7dccffedd45.jpg],origin[dam],category[],type[LOOKBOOK],res[w],hmver[1]&call=url[file:/product/main]"
+            title="Relaxed Fit Hoodie"
+            info=" Hoodie in sweatshirt fabric made from a cotton blend."
+            price="1,499"
+          />
+          <Product
+            image="//lp2.hm.com/hmgoepprod?set=source[/c3/08/c308e8eae26682c548bf8be6608bf7dccffedd45.jpg],origin[dam],category[],type[LOOKBOOK],res[w],hmver[1]&call=url[file:/product/main]"
+            title="Relaxed Fit Hoodie"
+            info=" Hoodie in sweatshirt fabric made from a cotton blend."
+            price="1,499"
+          />
+          <Product
+            image="//lp2.hm.com/hmgoepprod?set=source[/c3/08/c308e8eae26682c548bf8be6608bf7dccffedd45.jpg],origin[dam],category[],type[LOOKBOOK],res[w],hmver[1]&call=url[file:/product/main]"
+            title="Relaxed Fit Hoodie"
+            info=" Hoodie in sweatshirt fabric made from a cotton blend."
+            price="1,499"
+          />
+          <Product
+            image="//lp2.hm.com/hmgoepprod?set=source[/c3/08/c308e8eae26682c548bf8be6608bf7dccffedd45.jpg],origin[dam],category[],type[LOOKBOOK],res[w],hmver[1]&call=url[file:/product/main]"
+            title="Relaxed Fit Hoodie"
+            info=" Hoodie in sweatshirt fabric made from a cotton blend."
+            price="1,499"
+          />
+          <Product
+            image="//lp2.hm.com/hmgoepprod?set=source[/c3/08/c308e8eae26682c548bf8be6608bf7dccffedd45.jpg],origin[dam],category[],type[LOOKBOOK],res[w],hmver[1]&call=url[file:/product/main]"
+            title="Relaxed Fit Hoodie"
+            info=" Hoodie in sweatshirt fabric made from a cotton blend."
+            price="1,499"
+          />
         </div>
-        <div className="home__row"></div>
-        <div className="home__row"></div>
+        <div className="home__category">Trousers</div>
+        <div className="home__row">
+          <Product
+            image="//lp2.hm.com/hmgoepprod?set=source[/41/e8/41e8b5d478b68098437b9511686213d46d0e00ce.jpg],origin[dam],category[],type[DESCRIPTIVESTILLLIFE],res[w],hmver[2]&call=url[file:/product/main]"
+            title="Relaxed Fit Hoodie"
+            info=" Hoodie in sweatshirt fabric made from a cotton blend."
+            price="1,499"
+          />
+          <Product
+            image="//lp2.hm.com/hmgoepprod?set=source[/1f/e0/1fe0f78f8566f47cc65783668d9d9f1ffae26b43.jpg],origin[dam],category[],type[DESCRIPTIVESTILLLIFE],res[w],hmver[2]&call=url[file:/product/main]"
+            title="Relaxed Fit Hoodie"
+            info=" Hoodie in sweatshirt fabric made from a cotton blend."
+            price="1,499"
+          />
+          <Product
+            image="//lp2.hm.com/hmgoepprod?set=source[/50/3c/503c6226f76df04826594f2c963a7bf798c3f6d5.jpg],origin[dam],category[men_trousers_trousers_regular_all],type[DESCRIPTIVESTILLLIFE],res[w],hmver[2]&call=url[file:/product/main]"
+            title="Relaxed Fit Hoodie"
+            info=" Hoodie in sweatshirt fabric made from a cotton blend."
+            price="1,499"
+          />
+          <Product
+            image="//lp2.hm.com/hmgoepprod?set=source[/d4/60/d4600576336768dcf64f7e6201a5690db119aa84.jpg],origin[dam],category[],type[DESCRIPTIVESTILLLIFE],res[w],hmver[2]&call=url[file:/product/main]"
+            title="Relaxed Fit Hoodie"
+            info=" Hoodie in sweatshirt fabric made from a cotton blend."
+            price="1,499"
+          />
+          <Product
+            image="//lp2.hm.com/hmgoepprod?set=source[/6e/16/6e163ff4077a63fafaed6ae1e54ae79e0a137b72.jpg],origin[dam],category[],type[DESCRIPTIVESTILLLIFE],res[w],hmver[2]&call=url[file:/product/main]"
+            title="Relaxed Fit Hoodie"
+            info=" Hoodie in sweatshirt fabric made from a cotton blend."
+            price="1,499"
+          />
+        </div>
+        <div className="home__category">Accessories</div>
+        <div className="home__row">
+          <Product
+            image="//lp2.hm.com/hmgoepprod?set=source[/ff/3f/ff3f22d97172f4ec7a2e7ea0e3d7cb864c94e191.jpg],origin[dam],category[],type[DESCRIPTIVESTILLLIFE],res[w],hmver[2]&call=url[file:/product/main]"
+            title="Relaxed Fit Hoodie"
+            info=" Hoodie in sweatshirt fabric made from a cotton blend."
+            price="1,499"
+          />
+          <Product
+            image="//lp2.hm.com/hmgoepprod?set=source[/1f/e2/1fe288918cacc289dd8d75ffb9aea3cadd74af67.jpg],origin[dam],category[],type[DESCRIPTIVESTILLLIFE],res[w],hmver[2]&call=url[file:/product/main]"
+            title="Relaxed Fit Hoodie"
+            info=" Hoodie in sweatshirt fabric made from a cotton blend."
+            price="1,499"
+          />
+          <Product
+            image="//lp2.hm.com/hmgoepprod?set=source[/78/6b/786b7281b6d791e35c9660efe7188017fb6c7f16.jpg],origin[dam],category[],type[DESCRIPTIVESTILLLIFE],res[w],hmver[2]&call=url[file:/product/main]"
+            title="Relaxed Fit Hoodie"
+            info=" Hoodie in sweatshirt fabric made from a cotton blend."
+            price="1,499"
+          />
+          <Product
+            image="https://lp2.hm.com/hmgoepprod?set=source[/89/13/8913cc48b6187466bc6f5d158caa051e43d48acd.jpg],origin[dam],category[ladies_accessories_hatscarvesgloves],type[DESCRIPTIVESTILLLIFE],res[z],hmver[3]&call=url[file:/product/main]"
+            title="Relaxed Fit Hoodie"
+            info=" Hoodie in sweatshirt fabric made from a cotton blend."
+            price="1,499"
+          />
+          <Product
+            image="//lp2.hm.com/hmgoepprod?set=source[/a3/dc/a3dc4e97f6489074e75687a79732289256959f69.jpg],origin[dam],category[],type[DESCRIPTIVESTILLLIFE],res[w],hmver[2]&call=url[file:/product/main]"
+            title="Cotton twill cap"
+            info="Cap in cotton twill with an adjustable tab and metal fastener at the back"
+            price="499"
+          />
+        </div>
       </div>
     </div>
   );
--- src/Product.js
+++ src/Product.js
@@ -1,23 +1,21 @@
+import { Button } from '@material-ui/core';
 import React from 'react';
 import './Product.css';
 
-const Product = () => {
+const Product = ({ title, info, price, image }) => {
   return (
     <div className="product">
-      <img
-        className="product__image"
-        src="//lp2.hm.com/hmgoepprod?set=source[/c3/08/c308e8eae26682c548bf8be6608bf7dccffedd45.jpg],origin[dam],category[],type[LOOKBOOK],res[w],hmver[1]&call=url[file:/product/main]"
-        alt=""
-      />
+      <img className="product__image" src={image} alt="" />
       <div className="product__info">
-        <div className="product_infoLineOne">Relaxed Fit Hoodie</div>
-        <div className="product__infoLineTwo">
-          Hoodie in sweatshirt fabric made from a cotton blend.
-        </div>
+        <div className="product_infoLineOne">{title}</div>
+        <div className="product__infoLineTwo">{info}</div>
         <br />
         <div className="product_infoLineThree">
-          <div className="product__price">Rs. 1,499</div>
-          <div className="product__add">+ Add to Cart</div>
+          <div className="product__price">Rs. {price}</div>
+
+          <Button variant="outlined" className="product__add">
+            + Add to Cart
+          </Button>
         </div>
       </div>
     </div>
