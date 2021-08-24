73183bf18009136765336758d65c109032a289eb
Varun Khalate <khalatevarun@gmail.com>
2021-08-25 00:46:51 +0530

    add: display email at checkout page

--- src/Checkout.js
+++ src/Checkout.js
@@ -6,13 +6,14 @@ import CheckoutProduct from './CheckoutProduct';
 import { useStateValue } from './StateProvider';
 
 function Checkout() {
-  const [{ basket }, dispatch] = useStateValue();
+  const [{ basket, user }, dispatch] = useStateValue();
 
   return (
     <div className="checkout">
       <div className="checkout__left">
         <img className="checkout__ad" src={ad} alt="" />
         <div>
+          <h3>Hello, {user?.email}</h3>
           <h2 className="checkout__title">Your Shopping Basket</h2>
           {basket.map((item) => (
             <CheckoutProduct
