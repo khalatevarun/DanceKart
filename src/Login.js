d997f3dbbfba7d3ac85d511bece947faa4c9057c
Varun Khalate <khalatevarun@gmail.com>
2021-08-25 00:10:14 +0530

    add: redirect user to home page on successful registration

--- src/Login.js
+++ src/Login.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { auth } from './firebase';
 import './Login.css';
 
 function Login() {
+  const history = useHistory();
+
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
@@ -18,6 +20,9 @@ function Login() {
       .createUserWithEmailAndPassword(email, password)
       .then((auth) => {
         console.log(auth);
+        if (auth) {
+          history.push('/');
+        }
       })
       .catch((error) => alert(error.message));
   };
