83b8e3f6e6801a21bef149071af8291e891c2333
Varun Khalate <khalatevarun@gmail.com>
2021-08-29 19:40:45 +0530

    add: moment lib

--- src/SingleOrder.js
+++ src/SingleOrder.js
@@ -1,10 +1,24 @@
+import moment from 'moment';
 import React from 'react';
+import CheckoutProduct from './CheckoutProduct';
 import './SingleOrder.css';
 
 function SingleOrder({ order }) {
   return (
     <div className="order">
       <h2>Order</h2>
+      <p>{moment.unix(order.data.create).format('MMMM Do YYYY, h:mma')}</p>
+      <p className="order__id">
+        <small>{order.id}</small>
+      </p>
+      {order.data.basket?.map((item) => (
+        <CheckoutProduct
+          id={item.id}
+          price={item.price}
+          title={item.title}
+          image={item.image}
+        />
+      ))}
     </div>
   );
 }
