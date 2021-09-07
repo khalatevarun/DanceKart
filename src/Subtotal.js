6ed4de68e2e1ed10a26dca34ac78a145c8515ffc
Varun Khalate <khalatevarun@gmail.com>
2021-09-08 00:14:41 +0530

    add: address form

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -18,16 +18,13 @@ function Subtotal() {
               Subtotal ({totalItems} items):
               <strong>{value}</strong>
             </p>
-            <small className="subtotal__gift">
-              <input type="checkbox" /> This order contains a gift
-            </small>
           </>
         )}
         decimalScale={2}
         value={getBasketTotal(basket)}
         displayType={'text'}
         thousandSeparator={true}
-        prefix={'$'}
+        prefix={'INR'}
       />
       <button onClick={(event) => history.push('/payment')}>
         Proceed to Checkout
