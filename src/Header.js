c3ebf9a0f8e9f1f3ca14f54a3686e2da4fcd7ec0
Varun Khalate <khalatevarun@gmail.com>
2021-09-08 03:33:07 +0530

    add: add check on header is user logged in and greet accordingly

--- src/Header.js
+++ src/Header.js
@@ -35,7 +35,9 @@ const Header = () => {
       <div className="header__nav">
         <Link to={!user && '/login'}>
           <div className="header__option" onClick={handleAuthentication}>
-            <span className="header__optionLineOne">Hello Guest</span>
+            <span className="header__optionLineOne">
+              Hello {user ? 'Member' : 'Guest'}
+            </span>
             <span className="header__optionLineTwo">
               {user ? 'Sign Out' : 'Sign In'}
             </span>
